852323164bfaddc7aeccf92fda64eae9a99a8d8d
oopsguy <474608426@qq.com>
2017-08-23 21:05:22 +0800

    修改 request 的 Content-Type 引起的数据请求错误问题

--- DouBanBookApp/requests/request.js
+++ DouBanBookApp/requests/request.js
@@ -8,6 +8,9 @@ function request(url, data, successCb, errorCb, completeCb) {
     wx.request({
         url: url,
         method: 'GET',
+        header: {
+          'Content-Type': 'json'
+        },
         data: data,
         success: function(res) {
             if (res.statusCode == 200) {
